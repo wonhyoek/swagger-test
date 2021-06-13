6000ea54504d44f63c77ff8424ad89fee5783ba0
wonhyoek <epahsejtmxm@naver.com>
2021-06-13 10:38:18 +0900

    handle lowdb

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const low = require("lowdb");
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { Low, JSONFileSync } from "lowdb";
+
 
 const PORT = process.env.PORT || 5000;
 
-const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync("db.json");
-const db = new low(adapter);
+const adapter = new JSONFileSync("db.json");
+const db = new Low(adapter);
 
-db.defaults({books: []}).write();
+db.data = { books: [] }
 
 const app = express();
 
-app.db = db;
+app.db = db
 
 app.use(cors());
-app.use(express.json());
-app.use(morgan('dev'));
+app.use(express.json())
+app.use(morgan('dev'))
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
