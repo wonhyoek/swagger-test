3614c7f405078f3422f8dd9250913cad578dc0f2
wonhyoek <epahsejtmxm@naver.com>
2021-06-13 11:05:35 +0900

    set swagger

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import { Low, JSONFileSync } from "lowdb";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 
 const PORT = process.env.PORT || 5000;
@@ -12,6 +14,28 @@ const db = new Low(adapter);
 
 db.data = { books: [] }
 
+const options = {
+	definition: {
+        openapi: "3.0.0",
+        components: {},
+		info: {
+			title: "Library API",
+			version: "1.0.0",
+			description: "A simple Express Library API",
+		},
+		servers: [
+			{
+				url: "http://localhost:5000",
+			},
+		],
+	},
+	apis: ["./routes/*.js", "./swagger/*.yaml"]
+};
+
+const specs = swaggerJsDoc(options);
+
+
+
 const app = express();
 
 app.db = db
@@ -19,5 +43,6 @@ app.db = db
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
